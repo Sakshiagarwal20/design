13153bffd94765bc6a4db12afcf7139692acfccc
Sakshi <sakshi8125@gmail.com>
2020-03-09 15:33:09 +0530

    pattern

---
+++ exp.java
@@ -0,0 +1,26 @@
+public static <T> List<T> asList(T[] a) {
+    return new MyArrayList<T>(a);
+}
+
+private static class MyArrayList<T> extends AbstractList<T> {
+
+    private final T[] a;
+
+    MyArrayList(T[] array) {
+        a = array;
+    }
+
+    public T get(int index) {
+        return a[index];
+    }
+
+    public T set(int index, T element) {
+        T oldValue = a[index];
+        a[index] = element;
+        return oldValue;
+    }
+
+    public int size() {
+        return a.length;
+    }
+}
